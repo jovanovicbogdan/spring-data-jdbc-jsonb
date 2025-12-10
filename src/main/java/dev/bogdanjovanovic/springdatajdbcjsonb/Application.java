c425ca47b3c8ee4ab9dbb06ff42e5a4a5c272a47
Bogdan Jovanovic <jbogdan1998@hotmail.com>
2025-12-10 02:24:05 +0100

    feat: remove spring-webmvc dependency

--- src/main/java/dev/bogdanjovanovic/springdatajdbcjsonb/Application.java
+++ src/main/java/dev/bogdanjovanovic/springdatajdbcjsonb/Application.java
@@ -10,12 +10,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import tools.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 public class Application {
 
   static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public ObjectMapper objectMapper() {
+    return new ObjectMapper();
   }
 
   @Bean
